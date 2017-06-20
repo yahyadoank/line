b29b9e628ebd361934e1b8771e6b3100f7a907a0
yahya@email.com <yahya@email.com>
2017-06-20 16:56:22 +0700

    sdfaf

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -58,7 +58,7 @@ public class EchoApplication {
         String thanksConstants = "thanks|ok thanks|thank you|terima kasih|makasih|makasih ya|ok makasih";
         String askConstants = "apa|apa?|apanya|apanya?";
         String lieConstants = "bohong kau|boong kau|bener bohong|bener boong|iya aja sih|iya ajalah|iya gue bilang|iya gw bilang|tapi sepertinya iya kan";
-        String kasarConstants = "bodoh"|| "stupid" || "tolol" || "dungu" || "bego" || "anjing" || "fuck" || "asshole" || "bitch";
+        String[] kasarConstants = {"bodoh","stupid","tolol","dungu","bego","anjing","fuck","asshole","bitch"};
         String bodoConstants = "ga tau melulu|dasar payah|masa gitu aja ga tau|kok ngeselin sih|kok kzl ya";
         Random rand = new Random();
         int random = rand.nextInt(4);
